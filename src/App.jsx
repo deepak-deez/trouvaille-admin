702f6be15b87fb711d302b4c17963bdb8b3dce62
zenia banerjee <zenia@itobuz.com>
2023-04-21 18:54:34 -0700

    TROUV-9 Feat.:Signin module

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Router from "./Routes";
-import Child from "./components/adminLogin/adminLogin/adminLoginPortal.jsx.jsx";
+import Child from "./components/adminLogin/adminLogin/adminLoginPortal.jsx";
 
 const App = () => {
   return <Child />;
