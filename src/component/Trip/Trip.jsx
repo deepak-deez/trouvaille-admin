af67d5208ccadb7ef1f264a49f7fb23031536fa4
Sabir-itobuz <sabir@itobuz.com>
2023-04-27 17:57:25 +0530

    Feat: Added Trip page,Card,Add New Trip

---
+++ src/component/Trip/Trip.jsx
@@ -0,0 +1,48 @@
+import { React, useState } from "react";
+import TripDetails from "./tripData";
+import AddNewTrip from "../AddNewTrip/AddNewTrip";
+
+const Trip = () => {
+  const [tripPop, setTripPop] = useState(false);
+  return (
+    <>
+      <div className="w-full p-5">
+        <div className="flex justify-end">
+          <button
+            className="text-[#E75C54] font-bold"
+            onClick={() => {
+              setTripPop(!tripPop);
+              console.log("hii");
+            }}
+          >
+            Add Category
+            <i className=" ml-2 red-dot fa-solid fa-circle-plus"></i>
+          </button>
+        </div>
+        <div className="grid lg:grid-cols-4">
+          {TripDetails.map((data, index) => {
+            return (
+              <div className="w-full p-5 gap-4" key={index}>
+                <div className="p-8 bg-white rounded shadow-md">
+                  <div className="flex justify-end">
+                    <button>...</button>
+                  </div>
+                  <div className="flex justify-center">
+                    <img src={data.icon} alt="" />
+                  </div>
+                  <h3 className="text-center">{data.heading}</h3>
+                  <p className="text-gray-600 w-full md:h-[10vh] overflow-scroll">
+                    {data.description}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      {tripPop && <AddNewTrip tripPop={tripPop} setTripPop={setTripPop} />}
+    </>
+  );
+};
+
+export default Trip;
