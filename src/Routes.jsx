b8d56bf4acc857cd1f468750d45b385d4a43ff19
Sabir-itobuz <sabir@itobuz.com>
2023-05-02 18:00:47 +0530

    TROUV-52 Added Trip list From local storage

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/dashboard";
 import PageError from "./component/Error/PageError";
+import TripListPage from "./pages/tripListPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/list-of-trips" element={<TripListPage />} />
         <Route exact path="*" element={<PageError />} />
       </Routes>
     </BrowserRouter>
---
+++ src/component/TripList/TripList.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import TripListDB from "./TripListDB";
+
+const TripList = () => {
+  const [addPop, setAddPop] = useState(false);
+  return (
+    <>
+      <div className="p-3">
+        <div className="p-4 bg-white item-center w-full overflow-x-scroll border-b-2">
+          <table className="w-[65rem]">
+            <thead>
+              <tr className="tr-class">
+                <th className="p-3">Trip Title</th>
+                <th>Duration</th>
+                <th>Price</th>
+                <th>Discounted Price</th>
+                <th>
+                  <button
+                    className="text-[#E75C54]"
+                    onClick={() => {
+                      setAddPop(!addPop);
+                    }}
+                  >
+                    Add A New User
+                    <i className=" red-dot fa-solid fa-circle-plus"></i>
+                  </button>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {TripListDB.map((val, index) => {
+                return (
+                  <tr className=" tr-class text-center" key={index}>
+                    <td className="td-class font-bold flex justify-around items-center p-3">
+                      <img src={val.icon} alt="logo" />
+                      {val.title}
+                    </td>
+                    <td className="td-class">{val.duration}</td>
+                    <td className="td-class">{val.price}</td>
+                    <td className="td-class">{val.discounted}</td>
+                    <td className="">{/*  */}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TripList;
