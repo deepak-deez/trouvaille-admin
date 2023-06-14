7a8d7762e64bd84229472fe9cc66b29a55c99bf5
Sabir-itobuz <sabir@itobuz.com>
2023-06-14 15:54:04 +0530

    TROUV-10 FIX:Tabs design

--- src/components/BookingListTabs/BookingListTabs.jsx
+++ src/components/BookingListTabs/BookingListTabs.jsx
@@ -1,17 +1,57 @@
-import React from 'react'
+import React from "react";
+import "./style.scss";
 
 const BookingListTabs = (props) => {
-    const {setActiveStatusTab,activeStatusTab} = props
+  const { setActiveStatusTab, activeStatusTab } = props;
   return (
-    
-    <div className='grid grid-cols-5 p-2 bg-white font-semibold'>
-        <div className={"p-3 text-center " + (activeStatusTab ==="All"? "text-red-400 active":"")} onClick={()=>setActiveStatusTab("All") }>All</div>
-        <div className={"p-3 text-center " + (activeStatusTab ==="Confirmed"?"text-red-400 active":"")} onClick={()=>setActiveStatusTab("Confirmed")}>Confirmed</div>
-        <div className={"p-3 text-center " + (activeStatusTab ==="Pending"?"text-red-400 active":"")} onClick={()=>setActiveStatusTab("Pending")}>Pending</div>
-        <div className={"p-3 text-center " + (activeStatusTab ==="Cancelled"?"text-red-400 active":"")} onClick={()=>setActiveStatusTab("Cancelled")}>Cancelled</div>
-        <div className={"p-3 text-center " + (activeStatusTab ==="Completed"?"text-red-400 active":"")} onClick={()=>setActiveStatusTab("Completed")}>Completed</div>
+    <div className="grid grid-cols-5  bg-[#f5f9ff] font-semibold">
+      <div
+        className={
+          "p-3 text-center  " +
+          (activeStatusTab === "All" ? "text-red-400 active-tab" : "")
+        }
+        onClick={() => setActiveStatusTab("All")}
+      >
+        All
+      </div>
+      <div
+        className={
+          "p-3 text-center " +
+          (activeStatusTab === "Confirmed" ? "text-red-400 active-tab" : "")
+        }
+        onClick={() => setActiveStatusTab("Confirmed")}
+      >
+        Confirmed
+      </div>
+      <div
+        className={
+          "p-3 text-center " +
+          (activeStatusTab === "Pending" ? "text-red-400 active-tab" : "")
+        }
+        onClick={() => setActiveStatusTab("Pending")}
+      >
+        Pending
+      </div>
+      <div
+        className={
+          "p-3 text-center " +
+          (activeStatusTab === "Cancelled" ? "text-red-400 active-tab" : "")
+        }
+        onClick={() => setActiveStatusTab("Cancelled")}
+      >
+        Cancelled
+      </div>
+      <div
+        className={
+          "p-3 text-center " +
+          (activeStatusTab === "Completed" ? "text-red-400 active-tab" : "")
+        }
+        onClick={() => setActiveStatusTab("Completed")}
+      >
+        Completed
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BookingListTabs
+export default BookingListTabs;
